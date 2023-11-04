297b8b562b591fa3271fbc45307938efe948ff89
Julija <juleczek2002@gmail.com>
2023-11-04 19:37:40 +0100

    Trojki added

--- DodatkoweRozwiazania/trójki.py
+++ DodatkoweRozwiazania/trójki.py
@@ -11,6 +11,7 @@ def nwd(number1, number2):
     return number1
 
 
+#jedna przerwa obojętnie jakiej długości
 def trojki(tab):
     n = len(tab)
     counter = 0
@@ -18,13 +19,38 @@ def trojki(tab):
         if nwd(nwd(tab[i], tab[i+1]), tab[i+2]) == 1:
             print(tab[i], tab[i+1], tab[i+2])
             counter += 1
-    for i in range(4, n+1):  # 4
-        for j in range(n-i+1):  # 0
-            # tab[0], tab[1], tab[2 + 4 - 3]
+    for i in range(4, n+1):
+        for j in range(n-i+1):  
             if nwd(nwd(tab[j], tab[j+1]), tab[j+2+(i-3)]) == 1:
                 print(tab[j], tab[j+1], tab[j+2+(i-3)])
                 counter += 1
-            if nwd(nwd(tab[j], tab[j+1+(i-3)]), tab[j+2+(i-3)]) == 1:  # tab[0], tab[1+1], tab[3]
+            if nwd(nwd(tab[j], tab[j+1+(i-3)]), tab[j+2+(i-3)]) == 1: 
                 print(tab[j], tab[j+1+(i-3)], tab[j+2+(i-3)])
                 counter += 1
     return counter
+
+
+#jedna przerwa długości max 1
+def trojki2(tab):
+    n = len(tab)
+    counter = 0
+    for i in range(n-3+1):  # trójki obok siebie 123
+        if nwd(nwd(tab[i], tab[i+1]), tab[i+2]) == 1:
+            print(tab[i], tab[i+1], tab[i+2])
+            counter += 1
+    
+   
+    for i in range(n-4):# co dwa, np 1 3 5
+        if nwd(nwd(tab[i], tab[i+2]), tab[i+4]) == 1:
+            print(tab[i], tab[i+1], tab[i+2])
+            counter += 1
+            
+    for i in range (n-3): # jeden przerwy, np 1 2 4
+        if nwd(nwd(tab[i], tab[i+1]), tab[i+3]) == 1:
+            print(tab[i], tab[i+1], tab[i+3])
+            counter += 1
+        if nwd(nwd(tab[i], tab[i+2]), tab[i+3]) == 1:
+            print(tab[i], tab[i+2], tab[i+3])
+            counter += 1 
+        
+        
